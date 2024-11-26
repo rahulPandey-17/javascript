3f81e88f4cd10a3470b7acc8c1012ba0b9c3aefc
Rahul Pandey <rahul@localhost.localdomain>
2024-11-27 02:27:57 +0530

    data-types

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,17 @@
+"use strict" // treat all the js code as newer version
+
+let name = "Rahul Pandey";  // string
+let age = 24;  // number
+let isLoggedIn = true;  // boolean
+
+/*
+ * null => standalone value
+ * undefined => value is not assigned yet
+ * symbol => to identify uniqueness
+ * object => reference type(above all are primitive)
+*/
+
+// typeof - tells us the data-type of a variable
+
+console.log(typeof null); // it will show null as a object
+console.log(typeof undefined); // it will show undefined as undefined
---
+++ 01_basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 122334;
+let accountEmail = "rahul@google.com";
+var accountPassword = "12345";
+accountCity = "Chennai";  // not recommended at all
+let accountState;
+
+// accountId = 222222;  // not allowed
+
+accountEmail = "r@r.com";
+accountPassword = 223344;
+accountCity = "Jaipur";
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
