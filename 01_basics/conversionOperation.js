cc8c53061d97ca93fdd354ea8ee2a720ce2d044c
Rahul Pandey <rahul@localhost.localdomain>
2024-11-27 13:54:13 +0530

    string concatenation

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -1,5 +1,7 @@
 "use strict";
 
+/* *********Conversion********* */
+
 let score = "56";
 // console.log(typeof score);
 
@@ -21,16 +23,36 @@ let valueInNumber = Number(score);
 /* conversion to boolean */
 
 // 1
-console.log(Boolean(1));
+// console.log(Boolean(1));
 
 // 0
-console.log(Boolean(0));
+// console.log(Boolean(0));
 
 // empty string
-console.log(Boolean(""));
+// console.log(Boolean(""));
 
 // string
-console.log(Boolean("Rahul"));
+// console.log(Boolean("Rahul"));
 
 /* number to string */
-console.log(typeof String("100"));
+// console.log(typeof String("100"));
+
+
+/* **********Operations********** */
+
+let value = 10;
+let negativeValue = - value;
+console.log(negativeValue);
+
+let str1 = "hello";
+let str2 = ", Rahul";
+console.log(str1 + str2);
+
+/* string and number addition */
+
+// if string is the first value then the entire thing will be coverted to string
+console.log("1" + 0 + 0);
+
+// if numbers are first then noraml operations will be performed and then it is concatenated with string whenver a string is encountered
+// if a number is added to a string the whole thing becomes a string
+console.log(2 + 8 + "2" + 4);
